8713cad0fe608305418c20d9cfb17f13c2f323b1
Daniel Ramirez <danieldosdr@gmail.com>
2023-11-15 18:32:50 -0600

    Intro MVC

---
+++ spring-boot-web-mvc/src/main/java/mx/edu/uacm/TextosPropertiesConfig.java
@@ -0,0 +1,13 @@
+package mx.edu.uacm;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+	@PropertySource("classpath:textos.properties")
+})
+public class TextosPropertiesConfig {
+
+}
